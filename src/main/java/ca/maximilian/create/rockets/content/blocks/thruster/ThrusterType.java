d4df7438a078712d5c4284e13b60409b6a60edc2
maximilian1121 <latificlynch@gmail.com>
2026-05-06 19:20:08 -0600

    Tweak/Rebalance thrust amounts and spool times

--- src/main/java/ca/maximilian/create/rockets/content/blocks/thruster/ThrusterType.java
+++ src/main/java/ca/maximilian/create/rockets/content/blocks/thruster/ThrusterType.java
@@ -7,8 +7,8 @@ import net.minecraft.util.StringRepresentable;
 @Getter
 @RequiredArgsConstructor
 public enum ThrusterType implements StringRepresentable {
-    RAPTOR_3("raptor_3", new ThrusterStats(100, 0.001f, 0.001f)),
-    SATURN_V_F1("saturn_v_f1", new ThrusterStats(100, 0.001f, 0.001f));
+    RAPTOR_3("raptor_3", new ThrusterStats(200, 30f, 40f)),
+    SATURN_V_F1("saturn_v_f1", new ThrusterStats(400, 40f, 50f));
 
     private final String serializedName;
     private final ThrusterStats stats;
